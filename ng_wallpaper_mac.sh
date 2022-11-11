0f0f9974e0c7969ef192796aee1dfa30cb7d80df
Manavalan Gajapathy <manag@uab.edu>
2022-11-11 11:13:15 -0600

    refatcors to work in current osX

--- ng_wallpaper_mac.sh
+++ ng_wallpaper_mac.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Script modified from "https://gist.github.com/rachelss/f0cfb28b5cf290f5ab32"
 # Major modification is to use URL that is part of twitter info
@@ -10,34 +11,29 @@ mkdir -p ${WORK_DIR}
 cd ${WORK_DIR}
 mkdir -p temp pictures
 
-# Find Site to redirect to.
-INITIAL_SITE='temp/initial_site.html'
-curl -L "https://www.nationalgeographic.com/photography/photo-of-the-day/" -o ${INITIAL_SITE}
+# Save the site
+PAGE_URL="https://www.nationalgeographic.com/photography/photo-of-the-day/"
+SITE='temp/page.html'
+curl -L $PAGE_URL -o $SITE
 
 # Parsing HTML like this is a sin.
 # If it's no longer tagged with "canonical," script will break
-IMG_HTML=$(grep "canonical" ${INITIAL_SITE} | sed -e "s/.*href=\"//" -e "s/\".*//")
-echo "*** Redirect to:  $IMG_HTML ***"
-echo
-
-REDIRECTED_SITE="temp/redirected_site.html"
-curl -L $IMG_HTML -o ${REDIRECTED_SITE}
-open $IMG_HTML
-
-# Another hacky bit that will fail when Nat Geo changes their image naming conventions
-IMG_URL=$(grep "twitter:image:src" ${REDIRECTED_SITE} | grep -o 'http.*' | sed 's/..$//')
+IMG_URL=$(grep -o "twitter:image:src.*" $SITE | grep -o 'https.*' | sed -e 's/.*https\(.*\)jpg.*/https\1jpg/')
 echo "*** URL for pic of the day:  $IMG_URL ***"
 echo
 
-TITLE=$(grep "twitter:title" ${REDIRECTED_SITE} | sed -n -e 's/^.*content="//p' | sed -n -e 's/National Geographic.*//p')
-echo "*** Photo title:  ${TITLE} ***"
+# TITLE=$(grep "twitter:title" ${INITIAL_SITE} | sed -n -e 's/^.*content="//p' | sed -n -e 's/National Geographic.*//p')
+# echo "*** Photo title:  ${TITLE} ***"
 
 DATE=$(date +%Y-%m-%d)
-IMG_F="$(PWD)/pictures/${DATE}[${TITLE}].jpg"
+# IMG_F="$(PWD)/pictures/${DATE}[${TITLE}].jpg"
+IMG_F="$(PWD)/pictures/${DATE}.jpg"
 curl $IMG_URL -o "${IMG_F}"
 echo "*** Photo saved as:  ${IMG_F} ***"
 
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '${IMG_F}'"
-killall Dock
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${IMG_F}\""
+
+# open the page in browser
+open $PAGE_URL
 
 echo "DONE"
